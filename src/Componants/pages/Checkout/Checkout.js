16f984770d190698acfef99232ce2f162a60610d
munnisaikra <munnisaikra750@gmail.com>
2023-01-07 02:35:29 +0600

    checkout route added

--- src/Componants/pages/Checkout/Checkout.js
+++ src/Componants/pages/Checkout/Checkout.js
@@ -1,9 +1,79 @@
 import React from "react";
+import Nav from "../../Navbar/Nav";
 
 const Checkout = () => {
   return (
     <div>
+      <Nav />
+       <div className="flex items-center justify-center">
+       <form className="w-[400px]">
+        <div class="form-control w-full  mb-2">
+          <label class="label">
+            <span class="label-text">What is your name?</span>
+        
+          </label>
+          <input
+            type="text"
+            placeholder="Type here"
+            class="input input-bordered w-full "
+          />
+         
+        </div>
+        <div class="form-control w-full  mb-2">
+          <label class="label">
+            <span class="label-text">What is your name?</span>
+        
+          </label>
+          <input
+            type="text"
+            placeholder="Type here"
+            class="input input-bordered w-full "
+          />
+         
+        </div>
+        <div class="form-control w-full  mb-2">
+          <label class="label">
+            <span class="label-text">What is your name?</span>
+        
+          </label>
+          <input
+            type="text"
+            placeholder="Type here"
+            class="input input-bordered w-full "
+          />
+         
+        </div>
+        <div class="form-control w-full  mb-2">
+          <label class="label">
+            <span class="label-text">What is your name?</span>
+        
+          </label>
+          <input
+            type="text"
+            placeholder="Type here"
+            class="input input-bordered w-full "
+          />
+         
+        </div>
+        <div class="form-control w-full  mb-2">
+          <label class="label">
+            <span class="label-text">What is your name?</span>
+        
+          </label>
+          <input
+            type="text"
+            placeholder="Type here"
+            class="input input-bordered w-full "
+          />
+         
+        </div>
+     
       
+       
+       
+        <button type="submit" className="btn btn-primary text-white w-full">Checkout</button>
+      </form>
+       </div>
     </div>
   );
 };
